2ca577863c1b016c81e7de402321ab5e16a93f53
deebot10 <dee1jhill@gmail.com>
2021-06-14 10:46:59 -0600

    Write test for user story 2

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -15,14 +15,9 @@ RSpec.describe 'Plot Index' do
     visit '/plots'
   end
 
-    # User Story 1, Plots Index Page
-    # As a visitor
-    # When I visit the plots index page ('/plots')
-    # I see a list of all plot numbers
-    # And under each plot number I see names of all that plot's plants  
+    # User Story 1
   describe 'visit' do   
-    it 'shows a list of all the plot numbers' do
-      visit '/plots' 
+    it 'shows a list of all the plot numbers and plant names' do
       
       expect(page).to have_content('Plot Index')
 
@@ -34,5 +29,32 @@ RSpec.describe 'Plot Index' do
       expect(page).to have_content(@plant_2.name)
       expect(page).to have_content(@plant_3.name)
     end    
+            
+        #     User Story 2, Remove a Plant from a Plot
+        # As a visitor
+        # When I visit a plot's index page
+        # Next to each plant's name
+        # I see a link to remove that plant from that plot
+        # When I click on that link
+        # I'm returned to the plots index page
+        # And I no longer see that plant listed under that plot
+        # (Note: you should not destroy the plant record entirely)
+    it 'can destroy destroy a plant from the plot index ' do
+      garden = Garden.create!(name: "Dee Forest", organic: true) 
+
+      plot_1 = garden.plots.create!(number: 5, size: 'Large', direction: 'West')    
+
+      plant_1 = plot_1.plants.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90) 
+
+      visit '/plots'
+  
+      expect(page).to have_content('Remove Plant')      
+
+      click_link 'Remove Plant'
+
+      expect(current_path).to eq('/plots')
+
+      expect(page).to_not have_content(plant_1.name)
+    end
   end
 end
