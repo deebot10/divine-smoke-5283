52a6681f566fe18d97c89dfbccf1d4da8160bb00
deebot10 <dee1jhill@gmail.com>
2021-06-14 11:29:39 -0600

    Write test for user story 3

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe 'Gardern Show page' do
   before(:each) do
     @garden = Garden.create!(name: "Dee Forest", organic: true) 
 
-    @plot_1 = @garden.plots.create!(number: 5, size: 'Large', direction: 'West')    
-    @plot_2 = @garden.plots.create!(number: 50, size: 'Small', direction: 'North')    
-    @plot_3 = @garden.plots.create!(number: 10, size: 'Large', direction: 'East')    
+    @plot = @garden.plots.create!(number: 5, size: 'Large', direction: 'West')    
 
-    @plant_1 = @plot_1.plants.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90) 
-    @plant_2 = @plot_2.plants.create!(name: "Daisy", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
-    @plant_3 = @plot_3.plants.create!(name: "Orchard", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
+    @plant_1 = @plot.plants.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90) 
+    @plant_2 = @plot.plants.create!(name: "Daisy", description: "Prefers rich, well draining soil.", days_to_harvest: 80)
+    @plant_3 = @plot.plants.create!(name: "Orchard", description: "Prefers rich, well draining soil.", days_to_harvest: 200)
+    @plant_4 = @plot.plants.create!(name: "Daisy", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
+    @plant_5 = @plot.plants.create!(name: "Bonsai", description: "Prefers rich, well draining soil.", days_to_harvest: 50)
+    @plant_6 = @plot.plants.create!(name: "Heart Flower", description: "Prefers rich, well draining soil.", days_to_harvest: 110)
 
     visit garden_path(@garden)
   end
@@ -21,6 +22,14 @@ RSpec.describe 'Gardern Show page' do
     # Then I see a list of plants that are included in that garden's plots
     # And I see that this list is unique (no duplicate plants)
     # And I see that this list only includes plants that take less than 100 days to harvest  
-  it '' do
+  it 'shows a list of plants included in plots' do
+
+    expect(page).to have_content(@plant_1)
+    expect(page).to have_content(@plant_2)
+    expect(page).to have_content(@plant_5)
+    
+    expect(page).to_not have_content(@plant_3)
+    expect(page).to_not have_content(@plant_4)
+    expect(page).to_not have_content(@plant_6)
   end    
 end
